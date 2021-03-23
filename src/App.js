60b55febd3b20c207e8469724c5617446ed8f6e8
Kamsi Kodi <kamsikodi@gmail.com>
2021-03-23 21:06:18 +0100

    Updated login UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login.js';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player.js';
 
+const spotify = new SpotifyWebApi();
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token){
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => console.log(user)); 
+    }
+
+  }, []);
+
   return (
-    <div className="App">
-      
+    <div className="App">{token ? 
+        <Player /> : 
+        <Login />
+      }
     </div>
   );
 }
