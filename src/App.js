1f2270344df6c761d21ebb4612ed846c3205058f
Kamsi Kodi <kamsikodi@gmail.com>
2021-03-23 22:26:57 +0100

    Added color scheme

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Login from './Login.js';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './Player.js';
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 function App() {
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -16,16 +18,28 @@ function App() {
     const _token = hash.access_token;
 
     if (_token){
-      setToken(_token);
+      // setToken(_token);
       spotify.setAccessToken(_token);
-      spotify.getMe().then(user => console.log(user)); 
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token
+      });
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        });
+      }); 
+
+      
     }
 
   }, []);
 
+  console.log(token);
   return (
     <div className="App">{token ? 
-        <Player /> : 
+        <Player spotify={spotify} /> : 
         <Login />
       }
     </div>
--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,8 @@ export const initialState = {
     user: null,
     playlists: [],
     playing: false,
-    item: null
+    item: null,
+    token: "BQAZvYGdrM7oKhMfPBRGatvY76hGuT2BYV3j2bUbodCeBiPKCh_JLuwx-BCSSI4s1SOLXLzLXS7HL3acTS69pb-wfaSqoDUmf9jj24F53Lm6C6rJvtYRCh_UmWdRfsKVCX60mI_9i3M-a6zIqF4lPErTAWKnNgIRLeNIlQ6lfybfaIdHw3vf"
 }
 
 const reducer = (state, action) => {
@@ -13,6 +14,11 @@ const reducer = (state, action) => {
             return {
                 ...state, 
                 user: action.user
+            }
+        case 'SET_TOKEN': 
+            return {
+                ...state, 
+                token: action.token
             }
         default:
             return state;
